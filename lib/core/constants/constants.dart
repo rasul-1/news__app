a1a6272805ff12752e74d2f310f35684e372eb34
rasul-1 <muxtorovmuhammadrasul62@gmail.com>
2025-04-16 22:33:08 +0500

    dsa

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,5 @@
+const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = '094abddfda1944fb87d868677f3005ee';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
+const String kDefaultImage = "https://coffective.com/wp-content/uploads/2018/06/default-featured-image.png.jpg";
